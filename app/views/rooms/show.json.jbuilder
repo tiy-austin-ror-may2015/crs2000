ba36d52983e4d8a806d7109cdbd7b95e2c00fb99
tclewis91 <tclewis91@gmail.com>
2015-06-29 16:12:27 -0500

    added scaffold and devise for users

---
+++ app/views/rooms/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @room, :id, :name, :max_occupancy, :room_number, :imgurl, :location, :created_at, :updated_at
---
+++ db/migrate/20150629202839_create_rooms.rb
@@ -0,0 +1,14 @@
+class CreateRooms < ActiveRecord::Migration
+  def change
+    create_table :rooms do |t|
+      t.string :name
+      t.integer :max_occupancy
+      t.integer :room_number
+      t.string :imgurl
+      t.string :location
+      t.references :company
+
+      t.timestamps null: false
+    end
+  end
+end
