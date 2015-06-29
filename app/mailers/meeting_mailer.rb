e0d572dd10a45febdfe0472e298ebb76deeed8bb
Matthew Giardina <mlgiardina@me.com>
2015-06-29 17:11:37 -0500

    formatted meeting_scheduled html email

--- app/mailers/meeting_mailer.rb
+++ app/mailers/meeting_mailer.rb
@@ -3,6 +3,7 @@ class MeetingMailer < ApplicationMailer
   def meeting_scheduled(employee, admin)
     @employee = employee
     @admin = admin
+    @meeting = meeting
     recipients = [employee.email, admin.email]
 
     mail to: recipients, subject: "Meeting Scheduled"
