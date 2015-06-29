25d9dc858cb9412c7c78c9772fe59bc75608b2e0
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-29 18:27:31 -0500

    updated room update and room destroy action to check if user is an admin

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -24,7 +24,7 @@ class RoomsController < ApplicationController
   # POST /rooms
   # POST /rooms.json
  def create
-  # if user_is_admin? # replace with actual method
+  if user_is_admin?
 
       user = current_employee
       @company = user.company
@@ -40,32 +40,50 @@ class RoomsController < ApplicationController
       else
         redirect_to :back, alert: "Error occured, room not saved"
       end
-    # else
-    # redirect_to :back, alert: "Access Denied"
-    # end
+    else
+     redirect_to :back, alert: "Access Denied"
+    end
  end
 
   # PATCH/PUT /rooms/1
   # PATCH/PUT /rooms/1.json
   def update
-    respond_to do |format|
-      if @room.update(room_params)
-        format.html { redirect_to @room, notice: 'Room was successfully updated.' }
-        format.json { render :show, status: :ok, location: @room }
+      if user_is_admin?
+
+      user = current_employee
+      @company = user.company
+      @room    = @company.rooms.build
+
+      @room[:name]          = params[:room][:name]
+      @room[:location]      = params[:room][:location]
+      @room[:room_number]   = params[:room][:room_number]
+      @room[:imgurl]        = params[:room][:imgurl]
+      @room[:max_occupancy] = params[:room][:max_occupancy]
+      if @room.save
+        redirect_to :back, notice: "#{@room.name} has been updated"
       else
-        format.html { render :edit }
-        format.json { render json: @room.errors, status: :unprocessable_entity }
+        redirect_to :back, alert: "Error occured, updates not saved"
       end
+    else
+     redirect_to :back, alert: "Access Denied"
     end
   end
 
   # DELETE /rooms/1
   # DELETE /rooms/1.json
   def destroy
-    @room.destroy
-    respond_to do |format|
-      format.html { redirect_to rooms_url, notice: 'Room was successfully destroyed.' }
-      format.json { head :no_content }
+    if user_is_admin?
+      user = current_employee
+      company = user.company
+      @room = @company.rooms
+
+      @room.destroy
+      respond_to do |format|
+        format.html { redirect_to rooms_url, notice: 'Room was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    else
+      redirect_to :back, alert: "Access Denied"
     end
   end
 
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ActiveRecord::Base
 belongs_to :company
 has_many :meetings
-
+validates_presence_of :name, :max_occupancy, :location
  # def self.any_seats_available_at?(room_id, start_time, end_time)
  #    @room         = Room.find(room_id])
  #    room_capacity = @room.max_occupancy
