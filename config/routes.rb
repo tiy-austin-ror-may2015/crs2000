52640be6c93f52ac0483979fd72274fbfbb3f867
tclewis91 <tclewis91@gmail.com>
2015-06-29 21:43:18 -0500

    styling devise login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   resources :meetings
   devise_for :employees
+
+  devise_scope :user do
+  end
   resources :rooms
   resources :companies
 
-  root to:'application#index'
+  root to:'rooms#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
