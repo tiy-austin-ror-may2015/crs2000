98c61b858e969563b4daa5603f375e87e6c36b33
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-29 20:41:44 -0500

    admin dashboard also has today meeting count and all schedule meetings

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,7 +5,10 @@ class AdminController < ApplicationController
       @company = user.company
       @total_employees = @company.employees.count
       @total_rooms = @company.rooms.count
-      today = Time.now.strftime("%m/%d/%Y")
+      today = Time.now.strftime("%m/%d/%Y").gsub('/', '').to_i
+      @today_meetings = Meeting.where("start_time >= ? AND start_time <= ?",
+                                      Time.now.midnight - 1.day, Time.now.midnight)
+      @all_future_meetings = Meeting.where("start_time >= ?", Time.now.midnight - 1.day)
     else
       redirect_to :back, alert: "Access Denied"
     end
