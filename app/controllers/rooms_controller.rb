56a3a453ed0e327f13cfeeff501a5eae3623f22b
Benjamin Hand <gneeshot@gmail.com>
2015-06-30 13:54:15 -0500

    removed json and comments from rooms_controller, excluding json for reids index page

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,6 @@
 class RoomsController < ApplicationController
   before_action :set_room, only: [:show, :edit, :update, :destroy]
 
-  # GET /rooms
-  # GET /rooms.json
   def index
     update_rooms
     name = params.fetch("name", "")
@@ -32,24 +30,18 @@ class RoomsController < ApplicationController
     end
   end
 
-  # GET /rooms/1
-  # GET /rooms/1.json
   def show
     room = Room.find(params[:id])
   end
 
-  # GET /rooms/new
   def new
     @room = Room.new
     @all_rooms = Room.where(company_id: current_employee.company_id).pluck(:name)
   end
 
-  # GET /rooms/1/edit
   def edit
   end
 
-  # POST /rooms
-  # POST /rooms.json
  def create
   if user_is_admin?
 
@@ -71,8 +63,6 @@ class RoomsController < ApplicationController
     end
  end
 
-  # PATCH/PUT /rooms/1
-  # PATCH/PUT /rooms/1.json
   def update
       if user_is_admin?
 
@@ -94,17 +84,12 @@ class RoomsController < ApplicationController
     end
   end
 
-  # DELETE /rooms/1
-  # DELETE /rooms/1.json
   def destroy
     if user_is_admin?
       @company = employee_company
       @room = Room.find(params[:id])
       @room.destroy
-      respond_to do |format|
-        format.html { redirect_to rooms_url, notice: 'Room was successfully destroyed.' }
-        format.json { head :no_content }
-      end
+      redirect_to rooms_url, notice: 'Room was successfully destroyed.'
     else
       redirect_to :back, alert: "Access Denied"
     end
@@ -115,7 +100,6 @@ class RoomsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_room
       @room = Room.find(params[:id])
     end
@@ -129,7 +113,6 @@ class RoomsController < ApplicationController
       end
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def room_params
       params.require(:room).permit(:name, :max_occupancy, :room_number, :imgurl, :location)
     end
