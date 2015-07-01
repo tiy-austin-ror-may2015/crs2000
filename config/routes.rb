1bc0b9f30c64b6e191a1b474ada8dcc23656c47a
Benjamin Hand <gneeshot@gmail.com>
2015-07-01 11:17:36 -0500

    added in authenticate :employee block around all routes for global authentication

--- config/routes.rb
+++ config/routes.rb
@@ -60,25 +60,28 @@
 #
 
 Rails.application.routes.draw do
-  resources :meetings
-  post 'meetings/join/:id', to: 'meetings#join', as: :join_employee
   devise_for :employees
 
-  post 'meetings/invite/:meeting_id/:employee_id', to: 'meetings#invite', as: :invitation
-  get 'employees/:id', to: 'employees#show', as: 'employee'
-  get 'employees', to: 'employees#index', as: 'employees'
-  get 'employees/:id/edit', to: 'employees#edit', as: 'edit_employee'
-  patch 'employees/:id(.:format)', to: 'employees#update', as: 'patch_employee'
-  get '/search/meetings', to: 'meetings#search'
-  get '/search/rooms', to: 'rooms#search'
-  get '/search_advance/rooms', to: 'rooms#search_advance'
+  authenticate :employee do
+    resources :meetings
+    post 'meetings/join/:id', to: 'meetings#join', as: :join_employee
 
-  resources :rooms
-  resources :companies
-  get 'admin/dashboard', to: 'admin#dashboard', as: 'dashboard'
-  resources :admin
+
+    post 'meetings/invite/:meeting_id/:employee_id', to: 'meetings#invite', as: :invitation
+    get 'employees/:id', to: 'employees#show', as: 'employee'
+    get 'employees', to: 'employees#index', as: 'employees'
+    get 'employees/:id/edit', to: 'employees#edit', as: 'edit_employee'
+    patch 'employees/:id(.:format)', to: 'employees#update', as: 'patch_employee'
+    get '/search/meetings', to: 'meetings#search'
+    get '/search/rooms', to: 'rooms#search'
+    get '/search_advance/rooms', to: 'rooms#search_advance'
+
+    resources :rooms
+    resources :companies
+    get 'admin/dashboard', to: 'admin#dashboard', as: 'dashboard'
+    resources :admin
+  end
   root to:'rooms#index'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
