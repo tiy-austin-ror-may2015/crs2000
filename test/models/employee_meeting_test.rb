13a02d7e31b887f26dafd6c51ffbbdf044707900
Justin Herrick <justin@theironyard.com>
2015-07-01 08:28:49 -0500

    Annotating models and routes.

--- test/models/employee_meeting_test.rb
+++ test/models/employee_meeting_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: employee_meetings
+#
+#  id          :integer          not null, primary key
+#  enrolled    :integer          default(0)
+#  employee_id :integer
+#  meeting_id  :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class EmployeeMeetingTest < ActiveSupport::TestCase
