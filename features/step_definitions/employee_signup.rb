c991f74f71b91a9bd6b306037701a41bf284818d
Benjamin Hand <gneeshot@gmail.com>
2015-07-01 16:17:45 -0500

    created cucumber to test employee signup feature

---
+++ features/step_definitions/employee_signup.rb
@@ -0,0 +1,17 @@
+Given(/^I need to register$/) do
+  :authenticate_employee!
+end
+
+When(/^I sign up$/) do
+  visit '/employees/sign_up'
+  fill_in 'employee_name', with: 'joe joe'
+  fill_in 'employee_email', with: 'joe@joe.com'
+  fill_in 'employee_password', with: 'password'
+  fill_in 'employee_password_confirmation', with: 'password'
+  click_button("Sign Up")
+end
+
+Then(/^I should be on "(.*?)"$/) do |path|
+  assert_equal current_path, path
+end
+
