7bf9e08fbdcad22e61bf923c5be0524344279c06
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-30 21:51:04 -0500

    deleted unsused method in meeting model

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -4,21 +4,6 @@ class Meeting < ActiveRecord::Base
   has_many :employee_meetings
   has_many :invitations
 
-  def self.send_meetings(meetings)
-    all_meetings = [] # create container for meetings to send
-
-    meetings.each do |meeting| # loop thru meetings
-
-     if meeting.private # check if meeting is private
-      all_meetings << meeting if current_user.id == meeting.employee.id || user_is_admin?
-     else
-      all_meetings << meeting
-     end # cond
-    end # loop
-    all_meetings # send to display
-  end # send_meetings
-
-
   def self.search_for(query, search)
     self.where("lower(#{query}) LIKE ?", "%" + search.downcase + "%")
   end
