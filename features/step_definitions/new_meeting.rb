61612ee6c08151d84fd6c688a867b0034c04a744
Benjamin Hand <gneeshot@gmail.com>
2015-07-01 20:09:33 -0500

    working on adding feature to test new meetings being created

--- features/step_definitions/new_meeting.rb
+++ features/step_definitions/new_meeting.rb
@@ -1,11 +1,13 @@
-When(/^I create a meeting$/) do
-  Meeting.create(title: 'test', agenda: 'test',
-            start_time: '2016/07/09 19:29', end_time: '2016/07/09 19:29')
-  Room.create(name: "the test room")
-  visit '/meetings/new'
-  fill_in 'meeting_title', with: 'test meeting'
-  fill_in 'meeting_agenda', with: 'blah blah do this do that agenda'
-  fill_in 'meeting_start_time', with: '2016/07/09 19:29'
-  fill_in 'meeting_end_time', with: '2016/07/10 19:29'
-  click_button("Create Meeting")
-end
+# When(/^I create a meeting$/) do
+#   room = Room.create(name: "the test room", location: 'the moon')
+
+#   Meeting.create(title: 'test', agenda: 'test',
+#             start_time: '2016/07/09 19:29', end_time: '2016/07/09 19:29',
+#             room_id: room.id, employee_id: @employee.id)
+#   visit '/meetings/new'
+#   fill_in 'meeting_title', with: 'test meeting'
+#   fill_in 'meeting_agenda', with: 'blah blah do this do that agenda'
+#   fill_in 'meeting_start_time', with: '2016/07/09 19:29'
+#   fill_in 'meeting_end_time', with: '2016/07/10 19:29'
+#   click_button("Create Meeting")
+# end
