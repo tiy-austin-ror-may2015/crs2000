a058714a302a8b84b764f3f645369801beb083bc
Reid Paape <wrpaape@gmail.com>
2015-07-02 22:36:46 -0500

    stylized paginate buttons on rooms index page

--- app/assets/javascripts/components/rooms_table.js.jsx
+++ app/assets/javascripts/components/rooms_table.js.jsx
@@ -118,7 +118,11 @@ var Paginate = React.createClass({
 
 var PageButton = React.createClass({
   render: function () {
-    return(<a className='btn btn-default next-prev' onClick={ this.clicked } >{ this.props.page }</a>);
+    var className = 'btn btn-default next-prev pageButton'
+    if (this.props.grandparent.state.page === this.props.page) {
+      className = 'btn btn-primary next-prev pageButton'
+    };
+    return(<a className={ className } onClick={ this.clicked } >{ this.props.page }</a>);
   },
   clicked: function () {
     this.props.grandparent.setState({ page: this.props.page });
