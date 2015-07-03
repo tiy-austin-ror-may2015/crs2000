85760a9c6b3e12e8d1576e6b7753852093d505db
Reid Paape <wrpaape@gmail.com>
2015-07-02 21:53:55 -0500

    woops returned create in meetings controller to original state

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -99,6 +99,9 @@ class MeetingsController < ApplicationController
       @meeting.employee = current_employee
 
       if @meeting.save
+        em = EmployeeMeeting.new(meeting_id: @meeting.id, employee_id: current_employee.id)
+        em.save
+        MeetingMailer.meeting_scheduled(current_employee, @meeting).deliver_now
         redirect_to @meeting, notice: 'Meeting was successfully created.'
       else
         render :new
