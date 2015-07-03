3d670c70633a7a861cdc99781e83e616686d7eaf
Reid Paape <wrpaape@gmail.com>
2015-07-02 21:43:49 -0500

    matt clarified some things

--- app/models/employee_meeting.rb
+++ app/models/employee_meeting.rb
@@ -13,15 +13,8 @@ class EmployeeMeeting < ActiveRecord::Base
   belongs_to :employee
   belongs_to :meeting
   validates_with RoomForOneMore
-  after_create :email_invitee
 
   def self.attending(meeting)
     self.where(meeting_id: meeting).count
-  end
-
-  private
-
-  def email_invitee
-    MeetingMailer.meeting_scheduled(self.employee, self.meeting).deliver_now
   end
 end
