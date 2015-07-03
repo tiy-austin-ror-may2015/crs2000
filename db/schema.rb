dc5adfdd8273624a2e474f4cd46b58a3d5904d84
tclewis91 <tclewis91@gmail.com>
2015-07-03 08:58:36 -0500

    updated merge w misplaced buttons

--- db/schema.rb
+++ db/schema.rb
@@ -97,11 +97,13 @@ ActiveRecord::Schema.define(version: 20150701183308) do
     t.integer  "max_occupancy"
     t.integer  "room_number"
     t.string   "imgurl"
-    t.string   "location",                   null: false
+    t.string   "location"
     t.integer  "company_id"
-    t.integer  "meetings_count", default: 0
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.integer  "meetings_count",           default: 0
+    t.boolean  "available",                default: true
+    t.integer  "hours_until_next_meeting", default: 197731
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
   end
 
   add_foreign_key "amenities", "rooms"
